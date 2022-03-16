ad424dda826a3ce380cb9c3815e265655393af4b
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-03-17 02:11:10 +0600

    ex:01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// ex : 01
+
+function App(){
+  return(
+    <div className='App'>
+    <Person></Person>
+    <Person></Person>
+    <Person></Person>
+    <h1>Hey.....Ahhh!</h1>
+    <Friend></Friend>
+    <Friend></Friend>
+    <Friend></Friend>
+  </div>
+  )
 }
+
+function Person(){
+  return (
+    <div className='person'>
+      <h1>Md. Mofazzal Hossain</h1>
+      <p>Job Holder</p>
+    </div>
+  )
+}
+function Friend(){
+  return (
+    <div className='friend'>
+      <h1>Mohammad Mohitul Islam</h1>
+      <p>Jobber</p>
+    </div>
+  )
+} 
 
 export default App;
